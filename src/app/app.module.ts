23b33fc5e0dddb3f559cb37890d1ce7f2351191d
Javier Gonzalez <ashabefunker@gmail.com>
2020-05-21 22:26:44 -0500

    Avance del componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Componente0Component } from './componente0/componente0.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Componente0Component
   ],
   imports: [
     BrowserModule,
