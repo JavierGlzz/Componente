3c3bbae3cd32ead46e8e71bd82fdfd3be2e20c3d
Javier Gonzalez <ashabefunker@gmail.com>
2020-05-22 21:38:37 -0500

    Avance final

--- src/app/componente0/componente0.component.ts
+++ src/app/componente0/componente0.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./componente0.component.css']
 })
 export class Componente0Component implements OnInit {
+  
+  public italic = "fecha";
+  public title = "JavierGG";
+  public subtitle = "Javis";
+  public subtitleBold = "Por eso";
+  public messageButton = "Uno";
+  public numeroBolita = "2";
 
   constructor() { }
 
